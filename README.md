39aeaea27fb50da24ad03d697dcf6f56e02145e3
Erick Saavedra <esvdrah@gmail.com>
2022-04-08 22:25:20 -0500

    Add README.md and transition for Card component

---
+++ README.md
@@ -0,0 +1,49 @@
+# Weather Box
+
+Here you can see information about the current weather in your city.<br/>
+Available countries: United States, Peru, Colombia, Argentina, Mexico, Spain and Costa Rica,
+
+## Installation
+
+- First, clone this repository:
+
+```shell
+    $ git clone git@github.com:erick-sk/weather-box.git
+    $ cd weather-box
+```
+
+- Install packages and dev dependencies:
+
+```shell
+    $ npm install
+```
+
+## Available Scripts
+
+- Runs the app in the development mode:
+
+```shell
+    $ npm run dev
+```
+
+Open [http://localhost:3000](http://localhost:3000) to view it in your browser.
+
+- Builds the app for production to the dist folder:
+
+```shell
+    $ npm run build
+```
+
+It correctly bundles React in production mode and optimizes the build for the best performance.
+
+- Runs the app in preview mode:
+
+```shell
+    $ npm run preview
+```
+
+It is important to note that vite preview is intended for previewing the build locally and not meant as a production server.
+
+## API
+
+The Weather API: https://openweathermap.org/api
--- src/components/Card/index.jsx
+++ src/components/Card/index.jsx
@@ -1,4 +1,5 @@
 import './index.scss';
+import { motion } from 'framer-motion';
 
 const Card = ({ weather = {} }) => {
   const { name, main: { temp, temp_max, temp_min } = {} } = weather;
@@ -6,16 +7,23 @@ const Card = ({ weather = {} }) => {
   if (!name) return null;
 
   return (
-    <div className="card">
-      <div className="card-title">
-        <h2>The Weather in {name} is:</h2>
+    <motion.div
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      exit={{ opacity: 0 }}
+      transition={{ duration: 1 }}
+    >
+      <div className="card">
+        <div className="card-title">
+          <h2>The Weather in {name} is:</h2>
+        </div>
+        <div className="card-body">
+          <p className="temperature">{temp} Cº</p>
+          <p>Temperature min: {temp_min} Cº</p>
+          <p>Temperature max: {temp_max} Cº</p>
+        </div>
       </div>
-      <div className="card-body">
-        <p className="temperature">{temp} Cº</p>
-        <p>Temperature min: {temp_min} Cº</p>
-        <p>Temperature max: {temp_max} Cº</p>
-      </div>
-    </div>
+    </motion.div>
   );
 };
 
